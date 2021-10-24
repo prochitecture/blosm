ed045e4ee6f2be390e2022d5d3f7444e66c28576
Vladimir Elistratov <prokitektura+dev@gmail.com>
2021-10-24 19:41:42 +0200

    files for road polygons

---
+++ action/road_polygons.py
@@ -0,0 +1,12 @@
+
+
+
+class RoadPolygons:
+    
+    def __init__(self):
+        pass
+    
+    def do(self, manager):
+        for polyline in manager.polylines:
+            for edge in polyline.edges:
+                pass
