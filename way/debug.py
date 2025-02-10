a56ed5fb07649c6b05038ccfdb97f8ee8bfb3015
Vladimir Elistratov <613295+vvoovv@users.noreply.github.com>
2025-02-10 20:37:24 +0100

    debug utils for streets

--- way/debug.py
+++ way/debug.py
@@ -3,17 +3,15 @@ def printStreetContent(street, processBundle=False):
     if street.bundle and not processBundle:
         return
     
-    for item in street.iterItems():
-        if item.__class__.__name__ == "Intersection":
-            t = 0
-    
     print(
         "{0}:".format(street.id) +\
         " - ".join(
             (
-                "({0}/{1})".format(
-                    item.leftHead.item.head.tags["highway"] if item.leftHead else '-',
-                    item.rightHead.item.head.tags["highway"] if item.rightHead else '-'
+                "({0}({1})/{2}({3})".format(
+                    (item.leftHead.item.head.tags["highway"] if item.leftHead.leaving else item.leftHead.item.tail.tags["highway"]) if item.leftHead else '-',
+                    (item.leftHead.item.head.id if item.leftHead.leaving else item.leftHead.item.tail.id) if item.leftHead else '',
+                    (item.rightHead.item.head.tags["highway"] if item.rightHead.leaving else item.rightHead.item.tail.tags["highway"]) if item.rightHead else '-',
+                    (item.rightHead.item.head.id if item.rightHead.leaving else item.rightHead.item.tail.id) if item.rightHead else ''
                 )\
                 if item.__class__.__name__ == "Intersection" else "{0}({1})".format(item.__class__.__name__, item.id)
             )\
