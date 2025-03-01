d69b3b234d28622f8b18c3c96c597d82672705e9
Vladimir Elistratov <613295+vvoovv@users.noreply.github.com>
2025-03-01 22:16:16 +0100

    way debug tweaking

--- way/debug.py
+++ way/debug.py
@@ -7,13 +7,8 @@ def printStreetContent(street, processBundle=False):
         "{0}:".format(street.id) +\
         " - ".join(
             (
-                "({0}({1})/{2}({3})".format(
-                    (item.leftHead.item.head.tags["highway"] if item.leftHead.leaving else item.leftHead.item.tail.tags["highway"]) if item.leftHead else '-',
-                    (item.leftHead.item.head.id if item.leftHead.leaving else item.leftHead.item.tail.id) if item.leftHead else '',
-                    (item.rightHead.item.head.tags["highway"] if item.rightHead.leaving else item.rightHead.item.tail.tags["highway"]) if item.rightHead else '-',
-                    (item.rightHead.item.head.id if item.rightHead.leaving else item.rightHead.item.tail.id) if item.rightHead else ''
-                )\
-                if item.__class__.__name__ == "Intersection" else "{0}({1})".format(item.__class__.__name__, item.id)
+                "(Int)" if item.__class__.__name__ == "Intersection" else\
+                "{0}({1})({2})".format(item.__class__.__name__, item.id, "aeroway" if "aeroway" in item.tags else "highway")
             )\
             for item in street.iterItems()
         )
