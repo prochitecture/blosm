0e052084219d071dd9b46e73ef9038cdf96232bc
Vladimir Elistratov <613295+vvoovv@users.noreply.github.com>
2025-11-13 21:03:55 +0100

    commenting experimental code out

--- building2/renderer.py
+++ building2/renderer.py
@@ -120,6 +120,7 @@ class BuildingRendererNew(Renderer):
             )
             self.genPartFootprints.initFootprintAttributes()
         
+        """ FIXME: uncomment the code below or remove it if you don't need Geometry Nodes setup for buildings
         if self.app.preferMesh:
             # A key to the dictionary below is an object name as it's defined in <self.app.assetStore>.
             # A related value is tuple that consists of 
@@ -142,7 +143,7 @@ class BuildingRendererNew(Renderer):
             # a Blender collection for instances on the points cloud
             self.buildingAssetsCollection = bpy.data.collections[_collectionName]
             
-            """" FIXME: uncomment the code below or remove it if you don't need Geometry Nodes setup for buildings
+            
             # check if the Geometry Nodes setup with the name "blosm_gn_building" is already available
             _gnName, _gnCollection = "blosm_gn_building", "Collection Info"
             node_groups = bpy.data.node_groups
@@ -157,7 +158,7 @@ class BuildingRendererNew(Renderer):
                 os.path.join(os.path.dirname(self.app.baseAssetPath), "flat_roof_objects.blend"),
                 "blosm_flat_roof_objects"
             )
-            """
+        """
         
         if self.app.singleObject:
             for layer in self.app.layers:
