1ebafa5e28218c5493eac146af8c632cc2c8c85b
Vladimir Elistratov <613295+vvoovv@users.noreply.github.com>
2026-06-16 16:53:32 +0200

    v2.7.27

--- __init__.py
+++ __init__.py
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 bl_info = {
     "name": "Blosm",
     "author": "Vladimir Elistratov <prokitektura+support@gmail.com>",
-    "version": (2, 7, 26),
-    "blender": (4, 2, 0),
+    "version": (2, 7, 27),
+    "blender": (4, 5, 0),
     "location": "Right side panel > \"Blosm\" tab",
     "description": "A few clicks import of OpenStreetMap, Google 3D cities, terrain, satellite imagery, web maps",
     "warning": "",
